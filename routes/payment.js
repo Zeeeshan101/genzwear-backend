644a72578c33f9b37919114432f263293d7d4904
Zeeshan Khan <beybladder@gmail.com>
2025-08-15 02:01:16 +0530

    new change

--- routes/payment.js
+++ routes/payment.js
@@ -7,12 +7,14 @@ const router = express.Router();
 
 const razorpay = new Razorpay({
   key_id: process.env.RAZORPAY_KEY_ID,
-  key_secret: process.env.RAZORPAY_KEY_SECRET,
+  key_secret: process.env.RAZORPAY_KEY_SECRET
 });
 
 router.post("/create-payment-link", async (req, res) => {
   try {
     const { amount, name, email, phone, referenceId } = req.body;
+console.log("ENV Keys:", process.env.RAZORPAY_KEY_ID, process.env.RAZORPAY_SECRET);
+console.log("Body:", req.body);
 
    const paymentLink = await razorpay.paymentLinks.create({
   amount: amount * 100,
